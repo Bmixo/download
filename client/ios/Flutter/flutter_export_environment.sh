8391976c23cf3164e9771a9ef2f05f8015dd2c17
Bmixo <bmixo@M.lan>
2020-05-05 17:08:54 +0800

    .

--- client/ios/Flutter/flutter_export_environment.sh
+++ client/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/bmixo/program/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bmixo/go/src/github.com/Bmixo/Download/download"
+export "FLUTTER_APPLICATION_PATH=/Users/bmixo/go/src/github.com/Bmixo/download/client"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
